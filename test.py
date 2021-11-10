5e5a5323c79ce8b2768181fc27426f5a439c404b
Bradley Black <bradley_black@me.com>
2021-11-10 21:36:11 +1300

    Split the test function into three different tests
    Makes more sense that each test tests one thing rather than one large
    test that tests multiple things

--- test.py
+++ test.py
@@ -1,6 +1,10 @@
 import requests
 
 
+API = ('https://api.tmsandbox.co.nz/v1/Categories/6327/Details.json?'
+       'catalogue=false')
+
+
 def get_api_information(api):
     ''' Using the requests library, gets the information from the given
         api and returns the data formatted as JSON. If the request to get
@@ -19,49 +23,41 @@ def get_api_information(api):
     if expected_response_code in str(response_code):
         return response_code.json()
 
-    return {}
+    return None
 
 
-def test_data():
-    ''' Tests that the data found at a particular api matches the following
-        criteria, the Name must be "Carbon Credits", the CanRelist field must
-        be true, and an entry in a list with the key 'Promotions' must contain
-        the name 'Gallery', as well as a description that contains the text,
-        "Good position in category". This is done by initally requesting the
-        data, if the request is successful then the Name and CanRelist fields
-        are verified. If they are what is expected then the Promotions field
-        is iterated through to find the expected name and description. If those
-        are found then the test passes.
-
-        Args:
-            None
-
-        Returns:
-            None
-    '''
-
-    api = ('https://api.tmsandbox.co.nz/v1/Categories/6327/Details.json?'
-           'catalogue=false')
+def test_name():
     expected_name = "Carbon credits"
-    expected_name_promtions = "Gallery"
-    expected_description = "Good position in category"
-    data = get_api_information(api)
 
-    if len(data) < 1:
-        # Fail the test if we were unsuccessful in getting data from the api
-        # passed to get_api_information
-        assert False
+    data = get_api_information(API)
+
+    assert data is not None, f"Error fetching data from {API}"
 
     assert data['Name'] == expected_name
-    assert data['CanRelist'] is True
 
-    # Search the Promotions list for the expected name and expected description
+
+def test_can_relist():
+    expected_can_relist = True
+
+    data = get_api_information(API)
+
+    assert data is not None, f"Error fetching data from {API}"
+
+    assert data['CanRelist'] is expected_can_relist
+
+
+def test_promotions():
+    expected_name_promotions = "Gallery"
+    expected_description = "Good position in category"
+
+    data = get_api_information(API)
+
+    assert data is not None, f"Error fetching data from {API}"
+
     for entry in data['Promotions']:
-        if (entry['Name'] == expected_name_promtions and
+        if (entry['Name'] == expected_name_promotions and
                 expected_description in entry['Description']):
             assert True
             break
     else:
-        # If we do not break out of the loop, then name and description we are
-        # expecting was not found. In this case we want the test to fail
         assert False
