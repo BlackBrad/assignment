a70d1d9f4de5178baf43f54c1eda7a04bdce71d8
Bradley Black <bradley_black@me.com>
2021-11-10 20:26:01 +1300

    Add docstring to test function.
    Also fails the test if no matching name and description is found inside
    of the Promotions list.

--- test.py
+++ test.py
@@ -23,8 +23,21 @@ def get_api_information(api):
 
 
 def test_data():
-    '''
-        TODO: DO I NEED SOMETHING HERE?
+    ''' Tests that the data found at a particular api matches the following
+        criteria, the Name must be "Carbon Credits", the CanRelist field must
+        be true, and an entry in a list with the key 'Promotions' must contain
+        the name 'Gallery', as well as a description that contains the text,
+        "Good position in category". This is done by initally requesting the
+        data, if the request is successful then the Name and CanRelist fields
+        are verified. If they are what is expected then the Promotions field
+        is iterated through to find the expected name and description. If those
+        are found then the test passes.
+
+        Args:
+            None
+
+        Returns:
+            None
     '''
 
     api = ('https://api.tmsandbox.co.nz/v1/Categories/6327/Details.json?'
@@ -35,13 +48,20 @@ def test_data():
     data = get_api_information(api)
 
     if len(data) < 1:
-        # TODO: probably need to do something better than this!
+        # Fail the test if we were unsuccessful in getting data from the api
+        # passed to get_api_information
         assert False
 
     assert data['Name'] == expected_name
     assert data['CanRelist'] is True
 
+    # Search the Promotions list for the expected name and expected description
     for entry in data['Promotions']:
         if (entry['Name'] == expected_name_promtions and
                 expected_description in entry['Description']):
             assert True
+            break
+    else:
+        # If we do not break out of the loop, then name and description we are
+        # expecting was not found. In this case we want the test to fail
+        assert False
